2561a7865e6731a4b0eb9aa81714e17e20dfb364
Mateusz Wojtas <103265505+Matecek@users.noreply.github.com>
2025-08-18 16:39:57 +0200

    Dodanie edycji

--- app/src/Controller/BookController.php
+++ app/src/Controller/BookController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Repository\BookRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -32,6 +34,41 @@ class BookController extends AbstractController
             throw $this->createNotFoundException('Book not found');
         }
         return $this->render('book/show.html.twig', [
+            'book' => $book,
+        ]);
+    }
+
+    #[Route('/book/{id}/edit', name: 'book_edit', methods: ['GET', 'POST'])]
+    public function edit(int $id, BookRepository $books, Request $request, EntityManagerInterface $em): Response
+    {
+        $book = $books->find($id);
+        if (!$book) {
+            throw $this->createNotFoundException('Book not found');
+        }
+        if ($request->isMethod('POST')) {
+            $title       = (string) $request->request->get('title', '');
+            $author      = (string) $request->request->get('author', '');
+            $description = $request->request->get('description') ?: null;
+            $status      = (string) $request->request->get('status', 'reading');
+
+            if ($title === '' || $author === '') {
+                $this->addFlash('danger', 'Tytuł i autor są wymagane.');
+                return $this->render('book/edit.html.twig', ['book' => $book]);
+            }
+
+            $book->setTitle($title);
+            $book->setAuthor($author);
+            if (method_exists($book, 'setDescription')) {
+                $book->setDescription($description);
+            }
+            $book->setStatus($status);
+
+            $em->flush();
+
+            return $this->redirectToRoute('book_show', ['id' => $id]);
+        }
+
+        return $this->render('book/edit.html.twig', [
             'book' => $book,
         ]);
     }
